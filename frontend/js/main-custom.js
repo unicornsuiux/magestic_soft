208eeef1e9da3b1aed943ef3afdc694336c9035e
unicornsuiux <unicornsuiux@gmail.com>
2024-08-27 16:40:57 -0700

    working on home page

--- frontend/js/main-custom.js
+++ frontend/js/main-custom.js
@@ -294,6 +294,30 @@ $('.partner_slider').owlCarousel({
     }
 });
 
+jQuery(document).ready(function($) {
+    "use strict";
+    $('#customers-testimonials').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots:true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+          0: {
+            items: 1
+          },
+          768: {
+            items: 2
+          },
+          1170: {
+            items: 3
+          }
+        }
+    });
+});
 
 
 
